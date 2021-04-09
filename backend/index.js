60b72f8aeb52fc688593916cf2470accb08aa186
Herick Vinicius <herick.gustavo@gmail.com>
2021-04-08 23:48:53 -0400

    Changed some database configurations and added DOTENV dependency.

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv/config");
 
 const app = express();
 
-const port = process.env.PORT || 3000; // Verificar outra maneira de ler o .env.
+const port = process.env.SERVER_PORT; // Verificar outra maneira de ler o .env.
 
 app.listen(port, () => {
   console.log(`Executing on port ${port}.`);

365b590c6c207c6e7ce9446ec0f4b476f7b605bd
Luizdev <luizfernandovv043@gmail.com>
2021-04-12 21:06:40 -0400

    Fixed the authentication errors.

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,27 +1,23 @@
 const express = require("express");
+const { findOne } = require("../models/User");
 
 const User = require("../models/User");
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    console.log(req.body)
-    const { email } = req.body;
-
+  const { email } = req.body;
   try {
-    //Find in database email exists
-    //if (await User.findOne({ email })) {
-     //   return res.status(400).send({ error: "User alredy exists" });
-    
     const user = await User.create(req.body);
-
+    //if (await User.findOne({ email })){
+      //console.log(user);
+      //return res.status(400).send({ error: "User already exists" });
     //Don't show in finder to database
-    user.password = undefined;
-
+     delete user.password
     return res.send({ user });
-  } catch (err) {
-        console.log(err);
-        return res.status(400).send({ error: "Registration failed" });
+  }catch(err) {
+    console.log(err);
+    return res.status(400).send({ error: "Registration failed" });
   }
 });
 

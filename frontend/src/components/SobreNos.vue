1677774a1c3db68246760329f41286b602828b35
Ui Sam dos Santos <uisam.santos@gmail.com>
2021-04-16 04:12:20 -0300

    quem somos feito, falta arrumar algumas coisas no código e componentizar tudo

--- frontend/src/components/SobreNos.vue
+++ frontend/src/components/SobreNos.vue
@@ -1,40 +1,134 @@
 <template>
-	<div  id="sobre" class="all--sobre">
-		<div class="titulocolumns">
-			<h1 class="columns">Sobre Nós</h1>
-			<div class="barra--laranja--titulo columns"></div>
-		</div>
+  <div id="sobre" class="all--sobre">
+    <div class="titulo-com-barra">
+      <div class="container--titulo">
+        <div class="barra--laranja--titulo">
+          <h1 class="titulo">SOBRE NÓS</h1>
+        </div>
+      </div>
+    </div>
+    <div class="conteudo columns is-multiline ">
+      <div class="column is-4 is-offset-0">
+        <img
+          class="img-conteudo img-responsive"
+          src="@/assets/Jubileus-squad-fumdo.png"
+          alt="Logo do time"
+        />
+      </div>
+      <div class="column is-8">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl
+          rhoncus mattis rhoncus urna. Ornare aenean euismod elementum nisi quis
+          eleifend. Quis hendrerit dolor magna eget est. Odio pellentesque diam
+          volutpat commodo sed egestas egestas fringilla phasellus. Eu consequat
+          ac felis donec. Iaculis eu non diam phasellus.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div class="column is-full">
+        <p>
+          Missão: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+          do eiusmod tempor incididunt ut labore et dolore magna aliqua velit
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in.
+        </p>
+      </div>
+      <div class="column is-full is-offset-5">
+        <div class="barra--horizontal--paragrafos"></div>
+      </div>
 
-		<div>
-			<img src="" alt="">
-			<p></p>
-		</div>
+      <div class="column is-full">
+        <p>
+          Visão: Ut enimad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat sit amet, consectetur
+          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Nisl rhoncus mattis rhoncus urna. Ornare aenean euismod
+          elementum nisi quis.
+        </p>
+      </div>
+      <div class="column is-full is-offset-5">
+        <div class="barra--horizontal--paragrafos"></div>
+      </div>
 
-	</div>
+      <div class="column is-full">
+        <p>
+          Valores: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+          do eiusmod tempor incididunt ut labore et dolore magna aliqua velit
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in.
+        </p>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-.all--sobre{
-	height: 100vh;
+.all--sobre {
+  /* position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: center; */
+  height: 100vh;
+  padding: 0px 150px 0px 150px;
 }
-.titulo{
-	display: flex;
-	align-items: center;
-	justify-content: center;
+img.img-conteudo {
+  width: 500px;
+  height: 500px;
 }
-.barra--laranja--titulo{
-	position: relative;
-	width: 40px;
-	height: 0px;
-	top: 5px;
 
-	border: 5px solid #F9732C;
-	transform: rotate(90deg);
-	}
+.barra--horizontal--paragrafos {
+  margin-top: 80px;
+  width: 250px;
+  height: 0px;
+  left: 361px;
+  top: 596px;
+
+  border-top: 3px solid #f9732c;
+}
+
+/* Css do titulo com a barra */
+h1.titulo {
+  width: 236px;
+  margin-left: 18px;
+  font-family: Rambla;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 50px;
+  line-height: 61px;
+  color: black;
+}
+.titulo-com-barra {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom: 97px;
+}
+.container--titulo {
+  width: 200px;
+}
+
+.barra--laranja--titulo {
+  margin-top: 205px;
+  width: 10px;
+  height: 58px;
+  border-left: 5px solid #f9732c;
+}
+/* Css do titulo com a barra */
 </style>
--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -1,72 +1,64 @@
 <template>
-	<div class="all columns ">
-		<div class="column is-half">
-			<div class="texto-chamativo">
-				<p>
-					VOCÊ <br>
-					<span class="texto-amarelo">JÁ DEVE</span> <br>
-					TER OUVIDO<br>
-					<span class="texto-amarelo">FALAR DE</span> <br>
-					NÓS
-				</p>
-		</div>
-		<div class="column"></div>
-		</div>
-		<div class="column">
-			<div class="imagem-logo">
-				<img src="../assets/jubileus_squad.png" alt="Foto do mascote da jubileus com o nome da marca">
-			</div>
-		</div>
-	</div>
+  <div class="all columns ">
+    <div class="column is-half texto-chamativo">
+      <p>
+        VOCÊ <br />
+        <span class="texto-amarelo">JÁ DEVE</span> <br />
+        TER OUVIDO<br />
+        <span class="texto-amarelo">FALAR DE</span> <br />
+        NÓS
+      </p>
+    </div>
+    <div class="column imagem-logo">
+      <img
+        src="../assets/jubileus_squad.png"
+        alt="Foto do mascote da jubileus com o nome da marca"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+.all {
+  height: 110vh;
+  width: 100vw;
+  background-color: #181525;
+}
+.texto-chamativo {
+  display: flex;
+	align-items: center;
+	justify-content: center;
 
-	.all{
-		height: 100vh;
-		width: 100vw;
-		background-color: #181525;
+	align-self: center;
 
-	}
-	.texto-chamativo{
-		display: flex;
-		align-items: center;
-		justify-content: center;
+  height: 31.875rem;
+  color: var(--color-laranja);
+  margin-top: 50px;
+}
 
-		height: 31.875rem;
-		color:var(--color-laranja);
-		margin-top: 50px;
+.texto-chamativo p {
+  font-family: Rambla;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 65px;
+  line-height: 85px;
+}
 
-	}
+.texto-amarelo {
+  color: #d69b15;
+}
 
-	.texto-chamativo p {
+.imagem-logo {
+	text-align: center;
+	align-self: center;
+  margin-top: 40px;
+}
 
-		font-family: Rambla;
-		font-style: normal;
-		font-weight: normal;
-		font-size: 65px;
-		line-height: 85px;
-
-	}
-
-	.texto-amarelo{
-		color: #D69B15;
-	}
-
-	.imagem-logo{
-		margin-top: 40px;
-	}
-
-	.imagem-logo img {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: 31.875rem;
-	}
+.imagem-logo img {
+  height: 31.875rem;
+}
 </style>
